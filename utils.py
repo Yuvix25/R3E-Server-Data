eb4e075c454346e1d2bb5c294af28207a06fe0e8
Yuval Rosen <yuv.rosen@gmail.com>
2021-09-19 07:35:01 +0300

    fix bug

--- utils.py
+++ utils.py
@@ -56,7 +56,8 @@ def update_local_db(update_full_every=3, reset_small_every="friday"):
 
     now = int(time.time())
 
-    
+    if not os.path.isdir("./db"):
+        os.mkdir("db")
 
     # Users DB:
 
